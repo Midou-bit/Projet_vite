9f6255f951569b2866da941da3555d0445e74290
midou-bit <midou@DESKTOP-GEB5SA4>
2025-03-05 16:24:32 +0100

    exo11

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,21 @@ const dishes = [
   { name: "Tacos à l’unité", 
     price: 3, 
     image: "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg", 
-    isNew: true },
+    isNew: true,
+    inStock: true }, 
   { name: "Enchiladas", 
     price: 12, 
     image: "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg", 
-    isNew: false },
+    isNew: false,
+    inStock: false }, 
   { name: "Mole poblano", 
     price: 15, 
     image: "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg", 
-    isNew: false },
+    isNew: false,
+    inStock: true }, 
 ];
+
+const filteredDishes = dishes.filter(dish => dish.inStock); 
 
 function App() {
   return (
@@ -28,7 +33,7 @@ function App() {
       <main className="main-container">      
         <Container>
           <Row className="dish-row">
-            {dishes.map((dish, index) => (
+            {filteredDishes.map((dish, index) => (
               <Col key={index} lg={4} md={4} sm={12} className="d-flex justify-content-center">
                 <Dish {...dish} />
               </Col>
