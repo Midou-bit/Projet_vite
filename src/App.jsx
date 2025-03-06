040ac1f7e140da724262d11bc3756398a74068ce
midou-bit <midou@DESKTOP-GEB5SA4>
2025-03-06 12:24:11 +0100

    14.

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,15 @@ import Dish from './components/Dish';
 import "./assets/styles/App.scss";
 
 const dishes = [
-  { name: "Tacos à l’unité", price: 3, image: "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg", isNew: true, stock: 5 }, 
-  { name: "Enchiladas", price: 12, image: "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg", isNew: false, stock: 0 }, 
-  { name: "Mole poblano", price: 15, image: "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg", isNew: false, stock: 2 }, 
+  { name: "Tacos à l’unité",
+     price: 3, image: "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
+      isNew: true, stock: 5 }, 
+  { name: "Enchiladas",
+     price: 12, image: "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
+      isNew: false, stock: 0 }, 
+  { name: "Mole poblano", 
+    price: 15, image: "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
+     isNew: false, stock: 2 }, 
 ];
 
 function App() {
@@ -29,7 +35,7 @@ function App() {
   return (
     <>
       <Header cartCount={cartCount} />
-      
+
       <Navbar />
 
       <main className="main-container">      
