48e6ad6d232516bb8ed0576e95ab7782260c0ab7
midou-bit <midou@DESKTOP-GEB5SA4>
2025-03-07 11:28:11 +0100

    17

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Dish from './components/Dish';
 import "./assets/styles/App.scss";
-
+import { useContext } from 'react';
+import { CartContext } from './context/CartContext';
 
 const dishes = [
   { name: "Tacos à l’unité",
-     price: 3, image: "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
+     price: 3,
+     image: "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
       isNew: true, stock: 5 }, 
   { name: "Enchiladas",
-     price: 12, image: "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
+     price: 12,
+     image: "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
       isNew: false, stock: 0 }, 
-  { name: "Mole poblano", 
-    price: 15, image: "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
-     isNew: false, stock: 2 }, 
+  { name: "Mole poblano",
+     price: 15,
+     image: "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
+      isNew: false, stock: 2 }, 
 ];
 
 function App() {
   const [showNewOnly, setShowNewOnly] = useState(false);
+  const { cartCount } = useContext(CartContext);
+  const prevCartCountRef = useRef(cartCount);
+
+  useEffect(() => {
+    prevCartCountRef.current = cartCount;
+  }, [cartCount]);
 
   const handleShowNewOnly = () => {
     setShowNewOnly(prevState => !prevState);
@@ -31,11 +41,13 @@ function App() {
     <>
       <Header />
 
-      <main className="main-container">      
+      <main className="main-container">
         <Container>
           <Button variant="secondary" onClick={handleShowNewOnly} className="mb-3">
             {showNewOnly ? "Voir tout les plat" : "Nouveaut uniquement"}
           </Button>
+
+          <p>Le panier est passé de {prevCartCountRef.current} à {cartCount} articles.</p>
 
           <Row className="dish-row">
             {filteredDishes.map((dish, index) => (
